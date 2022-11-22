2691b3f7937e7c5452b20dc6af475b7b609a654f
Filipe Sales <eduardojs999@gmail.com>
2022-11-22 07:48:23 -0500

    feat: formulario de pratos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import PaginabaseAdmin from "./paginas/Administracao/PaginaBaseAdmin";
+import AdministracaoPratos from "./paginas/Administracao/Pratos/AdministracaoPratos";
+import FormularioPrato from "./paginas/Administracao/Pratos/FormularioPrato";
 import AdministracaoRestaurantes from "./paginas/Administracao/Restaurantes/AdministracaoRestaurantes";
 import FormularioRestaurante from "./paginas/Administracao/Restaurantes/FormularioRestaurante";
 import Home from "./paginas/Home";
@@ -11,13 +13,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
 
-
-      <Route path='/admin' element={<PaginabaseAdmin/>}>
-
-        <Route
-          path="restaurantes"
-          element={<AdministracaoRestaurantes />}
-        />
+      <Route path="/admin" element={<PaginabaseAdmin />}>
+        <Route path="restaurantes" element={<AdministracaoRestaurantes />} />
         <Route
           path="restaurantes/novo"
           element={<FormularioRestaurante />}
@@ -25,11 +22,10 @@ function App() {
         <Route
           path="restaurantes/:id"
           element={<FormularioRestaurante />}
-        ></Route>  
+        ></Route>
+        <Route path="pratos" element={<AdministracaoPratos />}></Route>
+        <Route path="pratos/novo" element={<FormularioPrato />}></Route>
       </Route>
-
-
-
     </Routes>
   );
 }
---
+++ src/paginas/Administracao/Pratos/FormularioPrato.tsx
@@ -0,0 +1,144 @@
+import { TagSharp } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  TextField,
+  Typography,
+  Container,
+  Toolbar,
+  Link,
+  Paper,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import http from "../../../http";
+import IRestaurante from "../../../interfaces/IRestaurante";
+import Itag from "../../../interfaces/ITag";
+
+const FormularioPrato = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`You have clicked ${count} times`);
+  });
+
+  const [nomePrato, setNomePrato] = useState("");
+  const [descricao, setDescricao] = useState("");
+
+  const [tag, setTag] = useState("");
+  const [tags, setTags] = useState<Itag[]>([]);
+
+  const [restaurante, setRestaurante] = useState("");
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+
+  const [imagem, setImagem] = useState<File | null>(null);
+
+  useEffect(() => {
+    http
+      .get<{ tags: Itag[] }>("tags/")
+      .then((resposta) => setTags(resposta.data.tags));
+    http
+      .get<IRestaurante[]>("restaurantes/")
+      .then((resposta) => setRestaurantes(resposta.data));
+  }, []);
+
+  const selecionarArquivo = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    if (evento.target.files?.length) {
+      setImagem(evento.target.files[0]);
+    } else {
+      setImagem(null);
+    }
+  };
+
+  const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+  };
+
+  return (
+    <>
+      <Box>
+        <Container maxWidth="lg" sx={{ mt: 1 }}>
+          <Paper sx={{ p: 2 }}>{/**Conteudo da pagina */}</Paper>
+        </Container>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: 4,
+          flexGrow: 1,
+        }}
+      >
+        <Typography component="h1" variant="h6">
+          Formulário de Pratos
+        </Typography>
+        <Box component="form" onSubmit={aoSubmeterForm}>
+          <TextField
+            value={nomePrato}
+            onChange={(evento) => setNomePrato(evento.target.value)}
+            label="Nome do Prato"
+            variant="standard"
+            fullWidth
+            required
+            margin="dense"
+          />
+          <TextField
+            value={descricao}
+            onChange={(evento) => setDescricao(evento.target.value)}
+            label="Descrição do Prato"
+            variant="standard"
+            fullWidth
+            required
+          />
+
+          <FormControl margin="dense" fullWidth>
+            <InputLabel id="select-tag">Tag</InputLabel>
+            <Select
+              labelId="select-tag"
+              value={tag}
+              onChange={(evento) => setTag(evento.target.value)}
+            >
+              {tags.map((tag) => (
+                <MenuItem key={tag.id} value={tag.id}>
+                  {tag.value}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl margin="dense" fullWidth>
+            <InputLabel id="select-restaurante">Restaurante</InputLabel>
+            <Select
+              labelId="select-restaurante"
+              value={restaurante}
+              onChange={(evento) => setRestaurante(evento.target.value)}
+            >
+              {restaurantes.map((restaurante) => (
+                <MenuItem key={restaurante.id} value={restaurante.id}>
+                  {restaurante.nome}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <input type="file" onChange={selecionarArquivo} />
+
+          <Button type="submit" variant="outlined" fullWidth>
+            Salvar
+          </Button>
+          <Button variant="outlined" onClick={() => setCount(count + 1)}>
+            Count
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default FormularioPrato;
